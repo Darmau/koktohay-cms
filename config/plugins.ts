2c2b100a0b78e9d72b7232e0e3627ff1cde956ca
李大毛 <dev@darmau.design>
2023-02-22 21:45:09 +0800

    set GraplQL always for now

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     config: {
       endpoint: '/graphql',
       shadowCRUD: true,
-      playgroundAlways: false,
+      playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
       apolloServer: {
